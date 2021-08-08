a3cfbdcb22cc64cbefa7b71e84403f1a446e858a
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-07 21:44:27 -0700

    post

--- src/main/java/dev/yw/whatsinme/ItemController.java
+++ src/main/java/dev/yw/whatsinme/ItemController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,14 @@ public class ItemController {
         return new ResponseEntity<>(
             itemRepository.findAll(),
             HttpStatus.OK
+        );
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Item> create(@RequestBody Item item){
+        return new ResponseEntity<>(
+            itemRepository.save(item), 
+            HttpStatus.CREATED
         );
     }
 
