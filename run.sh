ba09b28a7ecc1826c920ca1906b27c5b82cd76df
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2021-08-08 22:39:58 -0700

    logging

--- run.sh
+++ run.sh
@@ -6,27 +6,12 @@
 
 docker build -t yw/whatsinme-api . || exit 1
 
-case $1 in
-    "local")
-        docker run \
-            -e SERVER_PORT="$LOCAL_SERVER_PORT" \
-            -e DB_URL="$LOCAL_DB_URL" \
-            -e DB_USER="$LOCAL_DB_USER" \
-            -e DB_PASS="$LOCAL_DB_PASS" \
-            --rm \
-            --name whatsinme-api \
-            --network="host" \
-            yw/whatsinme-api
-    ;;
-    *)
-        docker run \
-            -e SERVER_PORT="$SERVER_PORT" \
-            -e DB_URL="$DB_URL" \
-            -e DB_USER="$DB_USER" \
-            -e DB_PASS="$DB_PASS" \
-            --rm \
-            -p ${REAL_PORT}:${SERVER_PORT} \
-            --name whatsinme-api \
-            yw/whatsinme-api
-    ;;
-esac
+docker run \
+    -e SERVER_PORT="$LOCAL_SERVER_PORT" \
+    -e DB_URL="$LOCAL_DB_URL" \
+    -e DB_USER="$LOCAL_DB_USER" \
+    -e DB_PASS="$LOCAL_DB_PASS" \
+    --rm \
+    --name whatsinme-api \
+    --network="host" \
+    yw/whatsinme-api
